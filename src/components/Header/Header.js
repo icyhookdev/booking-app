1b64f164703ef5e9a2f61b7f8363a99e25085f60
thienJs <cristian.albarran95@gmail.com>
2019-01-25 01:09:14 -0400

    customizing header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import classes from './Header.module.scss';
+import SearchForm from '../SearchForm/SearchForm';
 
 export default props => {
-  return <h1 className={classes.Header}>header</h1>;
+  return (
+    <header className={classes.Header}>
+      <h2>Booking App</h2>
+      <SearchForm />
+    </header>
+  );
 };
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './SearchForm.module.scss';
+
+export default props => (
+  <form className={classes.search}>
+    <input
+      className={classes.search__input}
+      type="text"
+      name="booking"
+      autoComplete="off"
+    />
+    <button className={classes.search__btn}>
+      <div className={classes.search__icon} />
+    </button>
+  </form>
+);
