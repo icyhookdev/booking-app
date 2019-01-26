0c540cf2a8133608f781060464472f30389e4de3
thienJs <cristian.albarran95@gmail.com>
2019-01-25 23:37:40 -0400

    header component finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import classes from './Header.module.scss';
+import { NavLink } from 'react-router-dom';
 import SearchForm from '../SearchForm/SearchForm';
 
 export default props => {
   return (
     <header className={classes.Header}>
-      <h2>Booking App</h2>
+      <h2>Booking place</h2>
       <SearchForm />
+      <nav className={classes.navbar}>
+        <ul className={classes.navbar__items}>
+          <li className={classes.navbar__item}>
+            <NavLink
+              activeClassName={classes.active}
+              className={classes.navbar__link}
+              to="/login"
+            >
+              Login
+            </NavLink>
+          </li>
+          <li className={classes.navbar__item}>
+            <NavLink
+              activeClassName={classes.active}
+              className={classes.navbar__link}
+              to="/signUp"
+            >
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
