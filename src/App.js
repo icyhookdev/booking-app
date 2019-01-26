a2d24f71bbdee6ee4957b441275f9aef67262edf
thienJs <cristian.albarran95@gmail.com>
2019-01-25 23:39:49 -0400

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Booking from './components/Bookings/Bookings';
 
 class App extends React.Component {
   render() {
-    return <Header />;
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Booking} />
+            <Route path="/login" />
+            <Route path="/register" />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
   }
 }
 
