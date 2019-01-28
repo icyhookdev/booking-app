1580abd70c8e8b8bebbe28094e240f3852b04776
thienJs <cristian.albarran95@gmail.com>
2019-01-27 20:02:39 -0400

    wire up redux and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxThunk from 'redux-thunk';
+
 import './index.css';
-
 import App from './App';
+import reducers from './reducers';
 
-render(<App />, document.querySelector('#root'));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(reduxThunk))
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
