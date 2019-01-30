99a64e0b8d046972a3ea104112f3a657655f41e1
thienJs <cristian.albarran95@gmail.com>
2019-01-30 00:41:27 -0400

    handling single rental display

--- src/reducers/rentalReducer.js
+++ src/reducers/rentalReducer.js
@@ -1,4 +1,4 @@
-import { GET_RENTALS } from '../actions/types';
+import { GET_RENTALS, GET_RENTAL } from '../actions/types';
 
 const initialState = {
   rentals: [],
@@ -10,6 +10,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_RENTALS:
       return { ...state, rentals: action.payload, loading: false };
+    case GET_RENTAL:
+      return { ...state, rental: action.payload, loading: false };
     default:
       return state;
   }
