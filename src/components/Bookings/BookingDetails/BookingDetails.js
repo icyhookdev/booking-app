408cd1f3eb2fe5785ff564df26eccda3bf804e8a
thienJs <cristian.albarran95@gmail.com>
2019-01-31 00:18:08 -0400

    redisign booking details

--- src/components/Bookings/BookingDetails/BookingDetails.js
+++ src/components/Bookings/BookingDetails/BookingDetails.js
@@ -17,12 +17,23 @@ class BookingDetails extends Component {
     return (
       <div className={classes.booking}>
         <div className={classes.booking__show}>
-          <div className={classes.booking__img}>
+          <div className={classes.img}>
             <img src={rental.image} alt="not found" />
           </div>
-          <div className={classes.booking__location} />
+          <div className={classes.location}>
+            <img src={rental.image} alt="not found" />
+          </div>
         </div>
-        <div className={classes.booking__details} />
+        <div className={classes.booking__details}>
+          <h2 className={classes.details__title}>{rental.title}</h2>
+          <p className={classes.details__city}>{rental.city}</p>
+          <ul className={classes.details__room}>
+            <li>{rental.bedrooms} bedrooms</li>
+            <li>{rental.bedrooms * 3} guests</li>
+            <li>{rental.bedrooms * 2} beds</li>
+          </ul>
+          <p className={classes.details__description}>{rental.description}</p>
+        </div>
       </div>
     );
   };
