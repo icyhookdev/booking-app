ba2a303e85ac9323ad22e691f43df04defb25926
thienJs <cristian.albarran95@gmail.com>
2019-01-31 00:18:39 -0400

    fixing a bug in the reducer

--- src/reducers/rentalReducer.js
+++ src/reducers/rentalReducer.js
@@ -9,7 +9,7 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_RENTALS:
-      return { ...state, rentals: action.payload, loading: false };
+      return { ...state, rentals: action.payload, loading: false, rental: {} };
     case GET_RENTAL:
       return { ...state, rental: action.payload, loading: false };
     default:
