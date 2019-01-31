38b997ac345f8c51a3f3c73a9557543c5cd65259
thienJs <cristian.albarran95@gmail.com>
2019-01-31 01:14:45 -0400

    making the map responsive

--- src/components/Bookings/BookingDetails/BookingDetails.js
+++ src/components/Bookings/BookingDetails/BookingDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getRental } from '../../../actions';
 import classes from './BookingDetails.module.scss';
+import { MapWithAMarker } from '../../Map/Map';
 
 class BookingDetails extends Component {
   componentDidMount() {
@@ -20,19 +21,24 @@ class BookingDetails extends Component {
           <div className={classes.img}>
             <img src={rental.image} alt="not found" />
           </div>
-          <div className={classes.location}>
-            <img src={rental.image} alt="not found" />
+          <div className={classes.booking__details}>
+            <h2 className={classes.details__title}>{rental.title}</h2>
+            <p className={classes.details__city}>{rental.city}</p>
+            <ul className={classes.details__room}>
+              <li>{rental.bedrooms} bedrooms</li>
+              <li>{rental.bedrooms * 3} guests</li>
+              <li>{rental.bedrooms * 2} beds</li>
+            </ul>
+            <p className={classes.details__description}>{rental.description}</p>
           </div>
         </div>
-        <div className={classes.booking__details}>
-          <h2 className={classes.details__title}>{rental.title}</h2>
-          <p className={classes.details__city}>{rental.city}</p>
-          <ul className={classes.details__room}>
-            <li>{rental.bedrooms} bedrooms</li>
-            <li>{rental.bedrooms * 3} guests</li>
-            <li>{rental.bedrooms * 2} beds</li>
-          </ul>
-          <p className={classes.details__description}>{rental.description}</p>
+        <div className={classes.location}>
+          <MapWithAMarker
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAOmEmG_JnHzjMFtAIg_4sB7k3tgWHHmlg&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
         </div>
       </div>
     );
